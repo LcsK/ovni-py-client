753b849c491c53c8499f660a9f4863792f1f386c
Lucas Lopes <lucas.ferreira@ibm.com>
2018-12-06 17:48:40 -0200

    Remove comments

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import requests
 
 resp = requests.get('https://ovni.herokuapp.com/api/apparition')
 if resp.status_code != 200:
-    # This means something went wrong.
     raise ApiError('GET /api/apparition/ {}'.format(resp))
 else:
     print('Apparitions: {} '.format(resp.json()))
